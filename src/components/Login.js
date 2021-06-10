d934183f15e0084dabec70e0b032df7ae919ea0a
fifinio <fiifnio13@gmail.com>
2021-06-10 20:40:35 +0200

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,26 +2,27 @@ import React,{useState} from 'react';
 
 
 
-const sendLogin = (login, password) => {
-
-    var payload = {
-        "identifier": login,
-        "password": password
-    };
-
-    var data = new FormData();
-    data.append( "json",  payload );
-
-    fetch("https://recruitment.ultimate.systems/auth/local",
-    {
-        method: "POST", 
-        body: data
-    })
-    .then(response => console.log(response))
-}
-
 const Login = () => {
 
+    const sendLogin = (login, password) => {
+    
+    console.log(login, password);
+    fetch("https://recruitment.ultimate.systems/auth/local", {
+  method: "POST",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(
+    {
+      identifier: "test1111",
+      password: "test123",
+    },
+  ),
+})
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err));
+}
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
 
@@ -31,12 +32,12 @@ const Login = () => {
             name="login" 
             placeholder="Email or Username" 
             type="text" required
-            onSubmit={e=>setLogin(e.target.value)}/>
+            onChange={e=>setLogin(e.target.value)}/>
             <input 
             name="password"
             placeholder="Password" 
             type="password" required
-            onSubmit={e=>setPassword(e.target.value)}/>
+            onChange={e=>setPassword(e.target.value)}/>
             <button onClick={sendLogin}>
                 Login
             </button>
